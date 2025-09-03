79bda81397ee3ebba1237336a46e5c2c2906b6f6
davimariete-tech <davi.mariete@ba.estudante.senai.br>
2025-09-03 16:59:28 -0300

    Add files via upload

---
+++ ativ.py
@@ -0,0 +1,29 @@
+import os
+os.system("cls")
+
+jan= 1
+fev=2
+marc=3
+
+
+
+numero=int(input("digite um numero de 1 a 12 para ver o mes correspondente:"))
+
+
+
+
+
+
+
+match 1 :
+         case 1:
+                print("voce escolheu jan")
+   
+match 2:
+         case 2:
+                print("voce escolheu fev")
+
+match 3:
+            case 3:
+
+             print("voce escolheu marc")
